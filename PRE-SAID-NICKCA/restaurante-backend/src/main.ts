f5e6b63c32720763253de55209268518bb718788
Valentino Cuenca Moreno <valentinocuen123@gmail.com>
2026-06-08 09:59:03 -0500

    PRESAID-D4-FINAL: Swagger + CHANGES.md + feedback + docs

--- PRE-SAID-NICKCA/restaurante-backend/src/main.ts
+++ PRE-SAID-NICKCA/restaurante-backend/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Restaurante API')
+    .setDescription('API del sistema de restaurante - PRE-SAID')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
